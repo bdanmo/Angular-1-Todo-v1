59977e118e31b6027970f41cd6db4c57c7b1e25c
Brendan Moran <b.daniel.moran@gmail.com>
2017-01-01 13:13:45 -0500

    Controllers for todo.name, todo.completed. HTML structuring of list item.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-angular.module("toDoList", []);
+angular.module("toDoList", [])
+.controller('mainCtrl', function($scope) {
+    $scope.helloWorld = function() {
+        console.log("Hello! This is the helloWorld funciton in the mainCtrl.");
+    }
+});
