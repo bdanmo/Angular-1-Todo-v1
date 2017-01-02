d1e6bd84f5a8c64bc5719d9028000976044e4356
Brendan Moran <b.daniel.moran@gmail.com>
2017-01-01 22:07:09 -0500

    Setup editing state. Added todos array.

--- js/app.js
+++ js/app.js
@@ -3,4 +3,11 @@ angular.module("toDoList", [])
     $scope.helloWorld = function() {
         console.log("Hello! This is the helloWorld funciton in the mainCtrl.");
     }
+
+    $scope.todos = [
+        {"name": "Practice guitar"},
+        {"name": "Learn more JavaScript"},
+        {"name": "Get a better job"},
+        {"name": "Record a song"},
+    ]
 });
